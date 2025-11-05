cd8a1b7fdfc1e7dc5460215464190079e791e351
Tehrence Llenarez <121592180+teejx@users.noreply.github.com>
2025-11-05 23:11:56 +0800

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,54 @@
+# day 1 - 30DaysOfPython Challenge
+
+#---------------------EXERCISE 2---------------------
+# 1. python version is Python 3.13.7
+
+# 2. Open the python interactive shell and do the following operations. The operands are 3 and 4.
+print ("addition:", 3 + 4)
+print ("subtraction:", 3 - 4)
+print ("multiplication:", 3 * 4)
+print ("modulus:", 3 % 4)
+print ("division:", 3 / 4)
+print ("exponential:",3 ** 4)
+print ("floor division", 3 // 4)
+print()
+
+# 3. Write strings on the python interactive shell. The strings are the following:
+print ("Tehrence Joie C. Llenarez")
+print ("Llenarez")
+print ("Philippines")
+print ("I am enjoying 30 days of python")
+print()
+
+
+# 4. Check the data types of the following data:
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Tehrence'))
+print(type('Llenarez'))
+print(type('Philippines'))
+print()
+
+
+#---------------------EXERCISE 3---------------------
+
+# 1. Write an example for different Python data types such as Number(Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary.
+print(type(21))
+print(type(2.1))
+print(type(2-1j))
+print(type('Sunny'))
+print(type(True))
+print(type(['Cloudy','Windy']))
+print(type((2,3,4,5,6)))
+print(type({1,3,7,9}))
+print(type({'name': 'teejx'}))
+print()
+
+# 2. Find an Euclidian distance between (2, 3) and (10, 8)
+a = (2,3)
+b = (10,8)
+# answer = 9.43
+
