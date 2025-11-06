ba16f35c3321540944489b0581bc625e9c8ce545
Tehrence Llenarez <121592180+teejx@users.noreply.github.com>
2025-11-07 01:00:03 +0800

    Create variables.py

---
+++ day2/variables.py
@@ -0,0 +1,61 @@
+# Day 2: 30 Days of python programming
+
+distance = (((10 - 2)**2) + ((8 - 3) **2)) ** 0.5
+print(distance)
+# Exercise 1
+first_name = 'Tehrence'
+last_name = 'Llenarez'
+full_name = 'Tehrence Llenarez'
+country = 'Philippines'
+city = 'Taguig'
+age = 22
+year = 2025
+is_married = False
+is_true = True
+is_light_on = True
+cat_name, dog_name, weather = 'Sunny','Moeby','Raining'
+print()
+
+# Exercise 2
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type({cat_name, dog_name, weather}))
+print()
+
+print(len(first_name))
+print(len(first_name),len(last_name))
+num_one, num_two = 5, 4
+total = (num_one + num_two)
+print(total)
+diff = (num_one - num_two)
+print(diff)
+product = (num_one * num_two)
+print(product)
+division = (num_one / num_two)
+print(division)
+remainder = (num_one % num_two)
+print(remainder)
+exp = (num_one ** num_two)
+print(exp)
+floor_division = (num_one // num_two)
+print(floor_division)
+
+radius = input('area: ')
+radius = int(radius)
+area_of_circle = (3.14) * (radius**2)
+circum_of_circle = (3.14 * radius * 2)
+print(radius,area_of_circle,circum_of_circle)
+fistname = input('first name: ')
+lastname = input('last name: ')
+country = input('country: ')
+age = input('age: ')
+
+distance = (((10 - 2)**2) + ((8 - 3) **2)) ** 0.5
