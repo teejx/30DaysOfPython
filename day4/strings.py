baf3501ab6e9e7be1594e1a6c2e85c8be5ab3d28
Tehrence Llenarez <121592180+teejx@users.noreply.github.com>
2025-11-13 12:44:12 +0800

    Update strings.py

--- day4/strings.py
+++ day4/strings.py
@@ -144,4 +144,21 @@ area = 3.14 * radius ** 2
 sentence = 'The area of a circle with radious {} is {:.0f} meters square.'.format(radius,area)
 print(sentence)
 
-# 36. Make other string formatting exercises as needed
+# 36. Make the following using string formatting methods:
+# 8 + 6 = 14
+# 8 - 6 = 2
+# 8 * 6 = 48
+# 8 / 6 = 1.33
+# 8 % 6 = 2
+# 8 // 6 = 1
+# 8 ** 6 = 262144
+
+a = 8
+b = 6
+print(f'{a} + {b} = {a +b}')
+print(f'{a} - {b} = {a - b}')
+print(f'{a} * {b} = {a * b}')
+print(f'{a} / {b} = {a / b:.2f}')
+print(f'{a} % {b} = {a % b}')
+print(f'{a} // {b} = {a // b}')
+print(f'{a} ** {b} = {a ** b}')
