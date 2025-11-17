5f498524b5f90dda006ff533037a6c328ceaac0e
Tehrence Llenarez <121592180+teejx@users.noreply.github.com>
2025-11-17 21:57:33 +0800

    Update conditionals.py

---
+++ day9/conditionals.py
@@ -0,0 +1,46 @@
+# -------------------
+# Exercises: Level 1
+# -------------------
+
+# 1.  Get user input using input("Enter your age: "). If user is 18 or older, give feedback: You are old enough to drive. 
+#     If below 18 give feedback to wait for the missing amount of years.
+
+
+# 2.  Compare the values of my_age and your_age using if … else. Use input("Enter your age: ") to get the age as input. 
+#     Print who is older and by how many years (handle year/years and equal case).
+
+
+# 3.  Get two numbers from the user using input(). Compare them and print whether a is greater, smaller, or equal to b.
+
+
+
+# -------------------
+# Exercises: Level 2
+# -------------------
+
+# 4.  Write a code which gives grade to students based on their scores:
+#     80-100 = A, 70-89 = B, 60-69 = C, 50-59 = D, 0-49 = F
+
+
+# 5.  Check if the input month belongs to Autumn, Winter, Spring, or Summer.
+
+
+# 6.  Using the fruits list, check if a fruit exists. If not, add it. If it exists, print a message.
+
+
+
+# -------------------
+# Exercises: Level 3
+# -------------------
+
+# 7.  Check if the person dictionary has a skills key. If so, print the middle skill.
+
+
+# 8.  Check if the person has the 'Python' skill and print the result.
+
+
+# 9.  Determine if the person is a front-end, back-end, or full-stack developer based on their skills.
+
+
+# 10.  If the person is married and lives in Finland, print:
+#      "<first_name> <last_name> lives in Finland. He is married."
